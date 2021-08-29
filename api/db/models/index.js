bcfc46bbb42c9e4164409084206093881cc71171
ajromeroch <ale.romero07@gmail.com>
2021-08-29 15:19:53 -0400

    entrega finall

--- api/db/models/index.js
+++ api/db/models/index.js
@@ -2,10 +2,4 @@ const S = require("sequelize");
 const Product = require("./Product");
 const Cart = require("./Cart");
 
-// Cart.hasMany(Product);
-// Product.belongsTo(Cart);
-
-Product.belongsToMany(Cart, { through: "ProductCart" });
-Cart.belongsToMany(Product, { through: "ProductCart" });
-
 module.exports = { Product, Cart };
